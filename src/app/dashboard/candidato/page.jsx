88b9330b84e7f1791839c7fd69a6ff7b518fae10
oscarquijano <oscar_gta45@hotmail.com>
2025-09-10 11:50:42 -0600

    Avance en dashboard postulante

--- src/app/dashboard/candidato/page.jsx
+++ src/app/dashboard/candidato/page.jsx
@@ -3,6 +3,6 @@
 export default function DashboardPage() {
     return (
         <div>
-            <h1>Dashboar de candidato - inicio</h1>
+            <h1>Dashboard de candidato - inicio</h1>
         </div>
     )}
